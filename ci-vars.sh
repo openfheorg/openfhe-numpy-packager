81dfc70da708e0bdcf7537342223c2615529de80
Dmitriy Suponitskiy <dsuponitskiy@dualitytech.com>
2025-10-21 12:37:57 -0400

    Update to v1.4.2.0

--- ci-vars.sh
+++ ci-vars.sh
@@ -1,8 +1,8 @@
 OS_NAME=Ubuntu
 OS_RELEASE=24.04
 # if you are changing OPENFHE_TAG (VERSION) here, then also adjust openfhe version in the block "install_requires" in setup.py
-OPENFHE_TAG=v1.4.0.2
-OPENFHE_PYTHON_TAG=v1.4.0.1.3
+OPENFHE_TAG=v1.4.2
+OPENFHE_PYTHON_TAG=v1.4.2.0
 OPENFHE_NUMPY_TAG=v1.4.0.4.2
 # subsequent release number for the given OPENFHE_NUMPY_TAG.
 WHEEL_MINOR_VERSION=0
